6074648a06c112750777d303daa949eda417a13c
Livio <hipzap@gmail.com>
2020-05-31 13:26:29 +0200

    Player Contolelr WIP

---
+++ Assets/Scripts/Gunscript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Gunscript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzFlash;
+    public GameObject impactEffect;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+
+    }
+    public void Shot()
+    {
+        //RaycastHit hit;
+        //if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        //{
+        //    Debug.Log(hit.transform.name);
+        //}
+
+        muzzFlash.Play();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            ITakeDamage damagable = hitInfo.collider.GetComponent<ITakeDamage>();
+            if (damagable != null)
+            {
+                damagable.ITakeDamage(damage);
+            }
+        }
+        Instantiate(impactEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour ,ITakeDamage
+
+{
+    [SerializeField]
+    private float health = 50f;
+
+    public void ITakeDamage(float amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
